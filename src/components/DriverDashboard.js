329bfec7eef1564863889c9a74dca35f10df055b
dhanush <dhanush.prabhu@outlook.com>
2024-12-08 19:06:48 +0530

    time updated to 5 sec

--- src/components/DriverDashboard.js
+++ src/components/DriverDashboard.js
@@ -29,7 +29,7 @@ const DriverDashboard = () => {
     if (isSharing) {
       locationInterval = setInterval(() => {
         navigator.geolocation.getCurrentPosition(getCurrentLocation);
-      }, 10000);
+      }, 5000);
     } else {
       clearInterval(locationInterval);
       recordLastLocation(driverId, lastLocation);
